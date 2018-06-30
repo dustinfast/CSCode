1d342e9836d89b4df16345e166f0e939b9cab17c
dustinfast <dustin.fast@hotmail.com>
2018-06-29 21:16:31 -0800

    Added resulting solution and project file structure -DF

---
+++ VSCodeCSDemo/src/VSCodeCSDemo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCodeCSDemo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
